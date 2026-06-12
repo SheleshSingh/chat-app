386407bea273869cbdec0de89b90a33d3be0d08f
Shelesh mathur <sheleshsingh755@gmail.com>
2026-06-12 16:10:38 +0530

    update cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,10 @@ app.listen(
     () => console.log(`Server is running on ${process.env.PORT}`)
 );
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT,
+    credentials: true
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
